dca278d8dfb19ada3587e9bd9cf98457ae6b8b47
Nitin Londhe <nitin.londhe@genmills.com>
2022-05-15 02:39:45 +0530

    added ens data pull logic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+	import '../app.css';
+	import { ethers } from 'ethers';
+	let address: string;
+	let ens: string;
+	let balance: string;
+    let network: string;
+    let err:string;
+
+    onMount(async ()=>{
+        network = await getNetwork();
+    });
+
+    async function getNetwork(): Promise<string> {
+        const provider = getProvider();
+        if (provider) {            
+            return (await provider.getNetwork()).name;
+        }
+    }
+
+    function getProvider(): ethers.providers.Web3Provider {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            return provider;
+        } else {
+            alert("Please Install Metamask");
+        }
+    }
+
+	async function getEns() {
+		try {
+            err = "";
+            network = await getNetwork();
+            const provider = getProvider();
+			if (provider) {			                
+				if (address) {
+					const bal = await provider.getBalance(address);
+					balance = ethers.utils.formatEther(bal);
+					ens = await provider.lookupAddress(address);
+                    
+				} else if (ens) {
+					const bal = await provider.getBalance(ens);
+					balance = ethers.utils.formatEther(bal);
+					address = await provider.resolveName(ens);
+				} else {
+                    alert("Please enter address or ens");
+                }
+			}
+		} catch (error) {
+            err = "not found";
+			console.log(error);
+		}
+	}
+</script>
+
+<div class="main">
+	<div>
+		<h1 class="title">Welcome to ens-resolver serrvice !</h1>
+		<div class="description">Enter Account address or ENS.</div>
+		<div>
+			Address :
+			<input type="text" bind:value={address} />
+		</div>
+		<div>
+			Ens :
+			<input type="text" bind:value={ens} />
+		</div>
+		<div>
+			Balance : {balance !== undefined ? balance : ""}
+		</div>
+        <div>
+            Network : {network !== undefined ? network : ""}
+        </div>
+        <div>
+            Error : {err !== undefined ? err : ""}
+        </div>
+		<button on:click={getEns}> Click me </button>
+	</div>
+	<div>
+		<img class="image" src="" />
+	</div>
+</div>
+
+<footer class="footer">Made with &#10084; by LearnWeb3 Punks</footer>
